8846e854deb17daabcecf24000e7a6b129d9a25b
danielcastro12 <129115300+danielcastro12@users.noreply.github.com>
2023-10-04 18:34:21 -0500

    Update NonexistentKeyException.java

--- FirstTaskCode/src/customExceptions/NonexistentKeyException.java
+++ FirstTaskCode/src/customExceptions/NonexistentKeyException.java
@@ -1,10 +1,1 @@
-package customExceptions;
 
-@SuppressWarnings("serial")
-public class NonexistentKeyException extends Exception{
-
-	public NonexistentKeyException(String message) {
-		super(message);
-		
-	}
-}
